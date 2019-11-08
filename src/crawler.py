7262ca75f21e56a2daac70da207d243ae7aeecbf
Siddhi Aphiphunya <sa14318@ic.ac.uk>
2019-11-08 21:47:24 +0000

    Implemented `getSource`.
    -> `getSource` retrieves full page source from site link provided.

--- src/crawler.py
+++ src/crawler.py
@@ -1,3 +1,6 @@
+import requests
+
+
 def regexedLinks(current):
 	pass
 
@@ -6,18 +9,28 @@ def uniqueEntries(found):
 	pass
 
 
+def getSource(start):
+	page = requests.get(start)
+	if (page.status_code > 400):
+		print("ERROR: " + page.status_code + " was reported.")
+		return []
+	else:
+		return page.text
+
+
 def main(start):
-	current = start
+	current = getSource(start)
 	nextIndexToSearch = 0
 	pruned = []
 	# len(n) of xs should be accessible in constant time O(1) as length is stored as a field alongside lists in python
 	while (len(pruned) < 100):
 		found = regexedLinks(current)
-		# uniqueEntries(xs) should consume found and empty list - adds unique entry to END of list
+		# uniqueEntries(xs) should consume `found` and empty list - adds unique entry to END of `pruned` list
 		pruned = uniqueEntries(found)
 		if (len(pruned) == nextIndexToSearch):
 			print("No more sites left to search, terminating process.\n")
-		current = pruned[nextIndexToSearch]
+			break
+		current = getSource(pruned[nextIndexToSearch])
 		nextIndexToSearch += 1
 
 		'''
@@ -25,3 +38,6 @@ def main(start):
 		ASSERT(UNIQUE(PRUNE))	
 		'''
 	return pruned
+
+
+main("https://news.ycombinator.com/")
